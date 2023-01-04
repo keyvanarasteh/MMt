a832cb17fea57dbc0c758a06af9a1b341bab82f9
sn-muratcan <sn.muratcan@hotmail.com>
2023-01-04 22:34:02 +0300

    Geliştirmeler yapılıyor. Deneme şablonu oluşturuluyor.

--- Workstation/Teststation/my_world/lib/screens/home_screen.dart
+++ Workstation/Teststation/my_world/lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -15,14 +15,45 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-          child: Text(
-        "Hello Isu Student",
-        style: TextStyle(
-          color: Colors.black54,
-          fontFamily: 'Merienda',
-          fontSize: 38,
-          fontWeight: FontWeight.w500,
-          fontStyle: FontStyle.italic,
+          child: SafeArea(
+        child: Column(
+          children: [
+            //Başlık Yazım
+            Text("Hello My BOSS.",
+                style: TextStyle(
+                    color: Colors.black87,
+                    fontFamily: 'Merienda',
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic)),
+            // Ana Teblerim
+            Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Container(color: Colors.blue, child: Text("1"))),
+                Expanded(
+                    flex: 2,
+                    child: Container(color: Colors.yellow, child: Text("2"))),
+                Expanded(
+                    flex: 5,
+                    child: Container(color: Colors.red, child: Text("3"))),
+                Expanded(
+                    flex: 2,
+                    child: Container(color: Colors.green, child: Text("4"))),
+              ],
+            ),
+            //Mesaj
+            Row(
+              children: [
+                Icon(Icons.chat),
+                SizedBox(
+                  width: 15,
+                ),
+                Text("Message"),
+              ],
+            )
+          ],
         ),
       )),
     );
